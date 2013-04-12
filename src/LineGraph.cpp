7c7e33c90928bab094a5a03aac79133571a895c0
Paweł Lampe <scony@sconysoft.com>
2013-04-12 13:56:35 +0200

    LineGraph fix

--- src/LineGraph.cpp
+++ src/LineGraph.cpp
@@ -2,6 +2,7 @@
 
 #include <fstream>
 #include <list>
+#include <string.h>
 
 using namespace std;
 
@@ -31,26 +32,73 @@ void LineGraph::readGraph(istream & in)
 {
   int ignore;
   string line;
-  list<string> l;
-  in >> ignore >> ignore >> ignore;
+  list<string> lst;
+  in >> ignore >> l >> ignore;
   while(in >> line)
-    l.push_back(line);
+    lst.push_back(line);
 
-  n = l.size();
+  n = lst.size();
   vLabel = new string[n];
   vMatrix = new int * [n];
   for(int i = 0; i < n; i++)
     vMatrix[i] = new int[n];
 
   int j = 0;
-  for(list<string>::iterator i = l.begin(); i != l.end(); i++)
+  for(list<string>::iterator i = lst.begin(); i != lst.end(); i++)
     vLabel[j++] = *i;
 
-  //build assoc's
+  for(int i = 0; i < n; i++)
+    for(int j = 0; j < n; j++)
+      {
+	int shift = nShifted(vLabel[i].c_str(),vLabel[j].c_str());
+	if(shift >= l)
+	  shift = 0;
+
+	vMatrix[i][j] = shift;
+      }
+}
+
+int LineGraph::nShifted(const char *a, const char *b)
+{
+    int n_common = 0;
+    int i;
+
+    for (i = 0; i < l; i++) {
+        if (strncmp(a + i, b, l - i) == 0) {
+            return i;
+        }
+    }
+    return l;
+}
+
+int LineGraph::getN()
+{
+  return n;
+}
+
+int LineGraph::getDistance(int a, int b)
+{
+  if(0 <= a && a < n && 0 <= b && b < n)
+    return vMatrix[a][b];
+  throw "Vertex out of bounds";
+}
+
+string LineGraph::getLabel(int a)
+{
+  if(0 <= a && a < n)
+    return vLabel[a];
+  throw "Vertex out of bounds";
 }
 
 void LineGraph::print()
 {
   for(int i = 0; i < n; i++)
     cout << vLabel[i] << endl;
+
+  for(int i = 0; i < n; i++)
+    {
+      for(int j = 0; j < n; j++)
+	cout << vMatrix[i][j] << " ";
+      cout << endl;
+    }
 }
--- src/LineGraph.hpp
+++ src/LineGraph.hpp
@@ -6,14 +6,20 @@
 class LineGraph
 {
   int n;
+  int l;
   std::string * vLabel;
   int ** vMatrix;
 
   void readGraph(std::istream & in);
+  int nShifted(const char *a, const char *b);
 public:
   LineGraph(std::istream & in);
   LineGraph(std::string fName);
   ~LineGraph();
+
+  int getN();
+  int getDistance(int a, int b);
+  std::string getLabel(int a);
 
   void print();
 };
