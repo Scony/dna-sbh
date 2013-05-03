4633bf2949390ebe9c2643820bfbac487bc81e36
Jakub Szwachła <meszkax@gmail.com>
2013-05-03 22:37:44 +0200

    Beginning of report.

--- run.sh
+++ run.sh
@@ -3,16 +3,16 @@
 #find tests/* | xargs -I {} sh -c 'cat {} | ./general.out'
 echo
 echo GROUP 1
-find tests/group1 -type f -exec sh -c './general.out < {}' \;
+find tests/group1 -type f -exec sh -c 'echo {}; ./general.out < {}' \;
 echo
 echo GROUP 2
-find tests/group2 -type f -exec sh -c './general.out < {}' \;
+find tests/group2 -type f -exec sh -c 'echo {}; ./general.out < {}' \;
 echo
 echo GROUP 3
-find tests/group3 -type f -exec sh -c './general.out < {}' \;
+find tests/group3 -type f -exec sh -c 'echo {}; ./general.out < {}' \;
 echo
 echo GROUP 4
-find tests/group4 -type f -exec sh -c './general.out < {}' \;
+find tests/group4 -type f -exec sh -c 'echo {}; ./general.out < {}' \;
 echo
 echo OWN
-find tests/own -type f -exec sh -c './general.out < {}' \;
+find tests/own -type f -exec sh -c 'echo {}; ./general.out < {}' \;
