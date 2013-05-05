b549c0897d6642c8145dfc30ba5ca01f4173c4df
Jakub Szwachła <meszkax@gmail.com>
2013-05-06 00:05:09 +0200

    Fix test order in run.sh. Save output of tests for negative errors in file.

--- run.sh
+++ run.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-#find tests/* | xargs -I {} sh -c 'cat {} | ./general.out'
-echo
+runtests() {
+for i in $(ls tests/${1} | sort -n); do
+    echo ${1}/$i; ./general.out $tests/${1}/$i;
+  done
+}
+
 echo GROUP 1
-find tests/group1 -type f -exec sh -c 'echo {}; ./general.out < {}' \;
+runtests group1
+
 echo
 echo GROUP 2
-find tests/group2 -type f -exec sh -c 'echo {}; ./general.out < {}' \;
+runtests group2
+
 echo
 echo GROUP 3
-find tests/group3 -type f -exec sh -c 'echo {}; ./general.out < {}' \;
+runtests group3
+
 echo
 echo GROUP 4
-find tests/group4 -type f -exec sh -c 'echo {}; ./general.out < {}' \;
+runtests group4
+
 echo
 echo OWN
-find tests/own -type f -exec sh -c 'echo {}; ./general.out < {}' \;
+runtests group4
